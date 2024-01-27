6e405154c7aefda184d47a6e6017bd07c47974af
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-01-27 15:33:43 -0500

    Add tests for completing Tasks

--- tests/components/TaskCard.test.tsx
+++ tests/components/TaskCard.test.tsx
@@ -1,13 +1,20 @@
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { cleanup, fireEvent, render, screen } from '@testing-library/react'
 import TaskCard from '../../app/components/TaskCard'
 import Task from '@/app/models/Task'
 
 const TASK: Task = {
-  id: 1,
+  id: 100,
   title: 'Test Task',
   description: 'Test Description',
   completed: false,
+}
+
+const COMPLETED_TASK: Task = {
+  id: 200,
+  title: 'Complete Task',
+  description: 'Task is complete',
+  completed: true,
 }
 
 test('renders TaskCard component', () => {
@@ -24,14 +31,46 @@ test('renders TaskCard component', () => {
     />,
   )
 
-  // Your existing assertions
-  expect(screen.getByText('Test Task')).toBeInTheDocument()
-  expect(screen.getByText('Test Description')).toBeInTheDocument()
+  expect(screen.getByText(TASK.title)).toBeInTheDocument()
+  expect(screen.getByText(TASK.description!)).toBeInTheDocument()
   expect(screen.getByRole('checkbox')).not.toBeChecked()
+  expect(screen.getByTitle('Delete Task')).toBeInTheDocument()
+  cleanup()
+
+  render(
+    <TaskCard
+      task={COMPLETED_TASK}
+      onComplete={handleComplete}
+      onDelete={handleDelete}
+      onUpdate={handleUpdate}
+    />,
+  )
+
+  expect(screen.getByText(COMPLETED_TASK.title)).toBeInTheDocument()
+  expect(screen.getByText(COMPLETED_TASK.description!)).toBeInTheDocument()
+  expect(screen.getByRole('checkbox')).toBeChecked()
   expect(screen.getByTitle('Delete Task')).toBeInTheDocument()
 })
 
-test('updates task title on click and blur', () => {
+test('TaskCard component updates completed status', () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleComplete = jest.fn((taskId) => {})
+
+  render(
+    <TaskCard
+      task={TASK}
+      onComplete={handleComplete}
+      onDelete={() => {}}
+      onUpdate={() => {}}
+    />,
+  )
+  const completedCheck = screen.getByRole('checkbox')
+  expect(completedCheck).not.toBeChecked()
+  fireEvent.click(completedCheck)
+  expect(handleComplete).toHaveBeenCalledWith(TASK.id)
+})
+
+test('Updates task title on click and blur', () => {
   const handleUpdate = jest.fn()
 
   render(
@@ -49,13 +88,13 @@ test('updates task title on click and blur', () => {
     target: { value: 'New Title' },
   })
   fireEvent.blur(screen.getByRole('textbox'))
-  expect(handleUpdate).toHaveBeenCalledWith(1, {
+  expect(handleUpdate).toHaveBeenCalledWith(TASK.id, {
     title: 'New Title',
     description: TASK.description,
   })
 })
 
-test('updates task description on click and blur', () => {
+test('Updates task description on click and blur', () => {
   const handleUpdate = jest.fn()
 
   render(
@@ -73,7 +112,7 @@ test('updates task description on click and blur', () => {
     target: { value: 'New Description' },
   })
   fireEvent.blur(screen.getByRole('textbox'))
-  expect(handleUpdate).toHaveBeenCalledWith(1, {
+  expect(handleUpdate).toHaveBeenCalledWith(TASK.id, {
     description: 'New Description',
     title: TASK.title,
   })
